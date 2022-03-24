04b436b6163adeb48dfdd68e9ff229767dadffd9
caioliveira277 <caio-henrique718@outlook.com>
2022-03-24 19:57:58 -0300

    adding new theme variables

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -4,6 +4,10 @@ interface IColors {
   white: string;
   shadow: string;
   primary: string;
+  secondary: string;
+  gray_400: string;
+  gray_500: string;
+  gray_600: string;
 }
 
 declare module 'styled-components' {
--- src/theme/default.ts
+++ src/theme/default.ts
@@ -5,5 +5,9 @@ export const theme: DefaultTheme = {
     white: '#ffffff',
     shadow: '#000000',
     primary: '#3CC6AA',
+    secondary: '#FDA506',
+    gray_400: '#868686',
+    gray_500: '#666666',
+    gray_600: '#535353',
   },
 };
