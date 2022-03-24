78cb3a3ccb3aa16f6e34b530b3ffc4ee7b533b16
caioliveira277 <caio-henrique718@outlook.com>
2022-03-23 22:35:25 -0300

    adding spash screen and icon

--- App.tsx
+++ App.tsx
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  async function prepare() {
+    try {
+      await SplashScreen.preventAutoHideAsync();
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      setAppIsReady(true);
+    }
+  }
+
+  useEffect(() => {
+    prepare();
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  return appIsReady ? (
+    <View 
+      style={styles.container}
+      onLayout={onLayoutRootView}
+    >
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  ): null;
 }
 
 const styles = StyleSheet.create({
