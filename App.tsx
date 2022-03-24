ac308a95667862f6134ab3ce1b31a9e81bdfab6b
caioliveira277 <caio-henrique718@outlook.com>
2022-03-23 23:23:30 -0300

    adding code patterns and pre-commit checking

--- App.tsx
+++ App.tsx
@@ -3,14 +3,21 @@ import { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
 
   async function prepare() {
     try {
       await SplashScreen.preventAutoHideAsync();
-    } catch (e) {
-      console.warn(e);
     } finally {
       setAppIsReady(true);
     }
@@ -27,21 +34,9 @@ export default function App() {
   }, [appIsReady]);
 
   return appIsReady ? (
-    <View 
-      style={styles.container}
-      onLayout={onLayoutRootView}
-    >
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar />
     </View>
-  ): null;
+  ) : null;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
