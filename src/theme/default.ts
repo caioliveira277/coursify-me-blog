123b62734441c348616cf7089b602b9026f577cd
caioliveira277 <caio-henrique718@outlook.com>
2022-03-24 16:59:23 -0300

    creating theme with styled components

---
+++ src/theme/default.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    white: '#ffffff',
+    shadow: '#000000',
+    primary: '#3CC6AA',
+  },
+};
