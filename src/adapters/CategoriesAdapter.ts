6124358c9a494ebbfa9a67a99ba7024726a86c29
caioliveira277 <caio-henrique718@outlook.com>
2022-03-24 20:57:00 -0300

    adding adapters

---
+++ src/adapters/CategoriesAdapter.ts
@@ -0,0 +1,18 @@
+import { AxiosResponse } from 'axios';
+import Constants from 'expo-constants';
+import { HttpClient } from './HttpClient';
+
+export interface IGetCategories {
+  id: number;
+  name: string;
+}
+
+export class CategoriesAdapter extends HttpClient {
+  public constructor() {
+    super(Constants.manifest?.extra?.BASE_API_URL);
+  }
+
+  public async getCategories(): Promise<AxiosResponse<IGetCategories[]>> {
+    return this.instance.get('/categories');
+  }
+}
